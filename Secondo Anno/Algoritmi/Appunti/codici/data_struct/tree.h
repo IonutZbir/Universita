0bda01d7815360a1fff6fac486082a7bfcb74566
ionut <ionut.roma9@gmail.com>
2023-11-20 17:40:48 +0100

    quasi finito la lezione del 14 di sistemi

--- Secondo Anno/Algoritmi/Appunti/codici/data_struct/tree.h
+++ Secondo Anno/Algoritmi/Appunti/codici/data_struct/tree.h
@@ -1,22 +1,22 @@
-typedef struct node{
+typedef struct node {
     int data;
-    struct node* father;
-    struct node* right;
-    struct node* left;
-}node;
+    struct node *father;
+    struct node *right;
+    struct node *left;
+} node;
 
-typedef struct binary_tree{
+typedef struct binary_tree {
     node *root;
     int len;
-}binary_tree;
+} binary_tree;
 
-typedef struct array{
+typedef struct array {
     int *data;
     int len;
-}array;
+} array;
 
 array *array_init();
 void print_array(array *);
-node *node_init(inti);
+node *node_init(int);
 binary_tree *binary_tree_init();
 void binary_tree_create(binary_tree *, int *);
